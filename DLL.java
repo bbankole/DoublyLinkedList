d72bd40f26c99370a0fae96a83b60a4fa3fc3fdb
bbankole <105624197+bbankole@users.noreply.github.com>
2026-02-17 15:35:11 -0500

    Update DLL.java
    Bridgette

--- DLL.java
+++ DLL.java
@@ -50,6 +50,62 @@ class DLL<E> {
     }
 
 
+    public int size() {
+        return this.counter;
+    }
+
+    public boolean isEmpty() {
+
+    //counter cannot be null, since it's a primitive type
+
+        if (this.counter == 0) {
+            return true;
+        } else {
+            return false;
+    } }
+
+    public E first() {
+
+        if (isEmpty()) {
+
+            return null;
+        }
+
+        return head.getElement();
+
+    }
+
+    public E last() {
+
+        if (isEmpty()) {
+            return null;
+        }
+
+        return tail.getElement();
+
+    }
+
+    public void addFirst(Node head, E newData) {
+
+
+        Node<E> newNode = new Node<>(element, null, head);
+
+        if(isEmpty()) {
+
+            head = newNode;
+            tail = newNode; 
+        }   // if the list is empty, this new list is the start and the end
+
+        else {
+            head.setPrev(newNode);
+
+            head = newNode;
+        }
+
+        counter++;
+        
+    }
+
 
 
 
